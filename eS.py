129a6895b67527347f631319eeb67bf31c9153b9
Quispie <Quispie@users.noreply.github.com>
2017-04-02 11:49:54 +0200

    Update eS.py
    Changed methods

--- eS.py
+++ eS.py
@@ -1,56 +1,76 @@
-class ES_integration:
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
+
+class testNaam:
 	""" 
 		Class function for using a ElasticSearch instance. 
+		
+		Author: Wesley Quispel
+		Student Number: 4014901
+
+		Last updated: 2017-04-01
+		
+		Usage:
+			
+			Initialise:
+			elastic = ES_integration()
+			
+			Clear previous MongoDB if database is updated:
+			elastic.clear_index(name) TODO
+
+			Populate from MongoDB:
+			elastic.index_docs(name, jsonvariable) TODO
+
+			Search queryterm:			
+			res = elastic.search(queryterm) TODO
+				@returns: list of n results consisting of sentences from the full text of the publications.
 	"""
 
-	import requests
-	from elasticsearch import Elasticsearch
-	import json
+	def __init__ (self):
+		import requests
+		from elasticsearch import Elasticsearch
+		import elasticsearch.helpers as ESH
+		import json
+		
+		# Name under which data is indexed
+		self.index_name = "mm"
 
-	def search(self, uri, term):
-	    """
-	    	Simple Elasticsearch Query
-	    """
-	    # Returns results starting with prefix term
-	    # Fuzzy allows for spelling mistakes to occur.
-	    query = self.es.search(index="sw", body={"query": {"fuzzy_like_this_field" : {"like_text": str(term), "max_query_terms":5}}})
-	    return query
+		# Amount of results for a query
+		self.query_returns = 5
 
-	def searchExpressive(self):
-		"""
-			Probably some more expressive search queries.
-		"""
+		# Document type name
+		self.doc_type_name = "docs"
 
-	def format_results(self, results):
-	    """
-	    	Print results nicely:
-	   			doc_id) content
-	    """
-	    data = [doc for doc in results['hits']['hits']]
-	    for doc in data:
-	        print("%s) %s" % (doc['_id'], doc['_source']['content']))
-
-	def index_doc(self, uri, doctype):
-	    """
-	    	Index new documents to ElasticSearch instance.
-	    """
-
-	    doctype = "docs"
-
-	    index = 1
-	    while this.res.status_code == 200:
-		    r = requests.get(str(uri)+str(index))
-		    self.es.index(index="sw", doc_type=doctype, id=index, body=json.loads(r.content))
-		    index = index + 1
-
-
-	def __main__():
-		"""
-			eS elasticSearch Class Constructor
-		"""
 		self.res = requests.get('http://localhost:9200')
-		self.es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+		self.es = Elasticsearch([{'host': 'localhost', 'port': 9200}])	
 
-	if __name__ == '__main__':
-		# execute only if run as the entry point into the program
-		main()
+	# __________________________________METHODS________________________________________________________________
+
+
+	def search(self, term):
+		"""
+			Simple Elasticsearch Query
+
+			"number_of_fragments" : 1
+		"""
+		# results = []
+		query_results =  self.es.search(index=self.index_name, body={"query": {"query_string": {"query":term}, "fields":["fulltext", "paragraphs", "_id", "title"]}, "number_of_fragments" : 1})
+		# for r in query_results:
+		# 	for n in r:
+		# 		if n.find(term):
+		return query_results
+
+	def clear_index(self):
+		"""
+			Clear the old index if new items need to be added. Call index_docs afterwards.
+		"""
+		self.es.delete(index=self.index_name)
+
+
+	def index_docs(self, json_file):
+		"""
+			Index new documents to ElasticSearch instance.
+		"""
+		self.es.index(index=self.index_name, doc_type=self.doc_type_name, id=1, body=json_file)
+
+		# self.ESH.bulk(self.es, json_file, index=self.index_name, doc_type=self.doc_type_name)
