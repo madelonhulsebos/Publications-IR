28f0a6e355053b1403b35bfe1cef3ff69dba602c
Madelon Hulsebos <m.hulsebos-1@student.tudelft.nl>
2017-04-08 02:50:54 +0200

    Train and output text files, trained classifier

--- Classifier_files/Classifier.py
+++ Classifier_files/Classifier.py
@@ -4,59 +4,36 @@ from nltk.tokenize import sent_tokenize
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.metrics.scores import accuracy
+from pymongo import MongoClient
+import unicodecsv as csv
+
+import nltk
+nltk.internals.config_java("C:\Program Files\Java\jdk1.7.0_80\bin\java.exe")
+
+import os
+java_path = "C:\Program Files\Java\jdk1.7.0_80\bin\java.exe"
+os.environ['JAVAHOME'] = java_path
 
 ### PUBLICATION TEXT PROCESSING
-
-wordssplitted = []
-# Split on word level
-for sentence in sentences
-	words = word_tokenize(sentence)
-	wordssplitted.append([words])
-
-# Stopword removal
-filtered_sentences = []
-stop_words = set(stopwords.words('english'))
-for sentence in wordssplitted
-	words = sentence
-	for w in words:
-		if w not in stop_words:
-			filtered_sentences.append([w])
-
-testfile = open(r'C:\Users\Madelon\Documents\Madelon\1. TU Delft\CS\1. MSc 1\Q3 Information Retrieval\Project\Trainingdata\Trainingdata.csv', 'wb')		
-wr = csv.writer(testfile, quoting=csv.QUOTE_NONNUMERIC)			
-			
-# For the first trained model: train on stemmed sentences
-ps = PorterStemmer()
-stemmed_sentences = [] # Retrieve testset
-for sentence in filtered_sentences:
-	for s in sentence:
-		stem_result = ps.stem(s)
-		stemmed_sentence = stemmed_sentence + stem_result
-	# Write this testset to a new file 
-	stemmed_sentences.append([stemmed_sentence])
-	wr.writerow(stemmed_sentence)
-	stemmed_sentence = []
+client = MongoClient('mongodb://localhost:27017/')
+database = client['IRDB_pubs']
+collection = database['Publications']
 	
-	
-### CLASSIFICATION PART - TEST & EVALUATION	(Classifier trained using Java)
-jar = 'C:/Users/Madelon/Downloads/stanford-ner/stanford-ner/stanford-ner.jar'
+# Trained the classifier with: 
+# java -cp stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -prop train.prop
 
-# Default model (Extracts persons, organizations, etc.)
-model = 'C:/Users/Madelon/Downloads/stanford-ner/stanford-ner/classifiers/english.all.3class.distsim.crf.ser.gz'
-# Trained model on stemmed sentences
-ownmodel_stemmed = 'C:/Users/Madelon/Downloads/stanford-ner/stanford-ner/classifiers/.......crf.ser.gz'
-# Trained model on complete sentences
-ownmodel_complete = 'C:/Users/Madelon/Downloads/stanford-ner/stanford-ner/classifiers/.......crf.ser.gz'
+# Accuracy of trained classifier
+#CRFClassifier tagged 8677 words in 288 documents at 5689,84 words per second.
+#         Entity P       R       F1      TP      FP      FN
+#              M 0,6694  0,1957  0,3028  81      40      333
+#         Totals 0,6694  0,1957  0,3028  81      40      333
 
-# Execute classifier on testset WITH STEMMING
-stNER_stem = StanfordNERTagger(ownmodel_stemmed, jar, encoding='utf8')
-classified_text_stem = stNER_stem.tag(stemmed_sentences)
+publications = collection.find({'journal':'I. J. Network Security'},{'journal':'IJCLCLP'})
+testfile = open(r'C:\Users\Madelon\Documents\Madelon\1. TU Delft\CS\1. MSc 1\Q3 Information Retrieval\Project\Trainingdata\NewMethodsFile.csv', 'wb')
+for pub in publications:
+	fulltext = pub['content.fulltext']
+	wr = csv.writer(testfile, quoting=csv.QUOTE_NONNUMERIC)	
+	wr.writerow([fulltext])
 
-# Execute classifier on testset WITHOUT STEMMING
-stNER_complete = StanfordNERTagger(ownmodel_complete, jar, encoding='utf8')
-classified_text_complete = stNER_complete.tag(sentences)
-
-# Testing Stanford NER Tagger for accuracy
-# Retrieve annotated sentences from test dataset
-reference_annotations = []
-accuracy = accuracy(reference_annotations, classified_text)
+# Executed the classifier on unseen publications
+# java -cp stanford-ner.jar edu.stanford.nlp.ie.crf.CRFClassifier -loadClassifier train_final.ser.gz -textFile NewMethodsText.txt
