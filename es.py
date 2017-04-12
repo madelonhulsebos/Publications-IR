7418a5e18c05aeffc9c5c60feb4828a3c99a1fa3
Quispie <wesley.quispel@gmail.com>
2017-04-12 11:25:43 +0200

    Added website etc.
    Should be final.

--- es.py
+++ es.py
@@ -14,33 +14,100 @@ import requests
 from elasticsearch import Elasticsearch
 
 # Initialise MongoDB connections
-client = MongoClient()
-db = client['Publications']
-collection = db['pub']
+# client = MongoClient()
+# db = client['Publications']
+# collection = db['pub']
 
 # Initialise ElasticSearch
 res = requests.get('http://localhost:9200')
 es = Elasticsearch([{'host': 'localhost', 'port': 9200}])	
 
-# Export documents from MongoDB
-# call("mongoexport --db Publications --collection pub --query '{\"journal\": \"IJCLCLP\"}' --out=test.json", shell=True)
+# Create an ElasticSearch index for the first run.
+# es.indices.create(index='my-index', ignore=400)
 
-# Open JSON file and append to list
+# # Export documents from MongoDB
+# call("mongoexport --db Publications --collection pub --query '{\"journal\": \"IJCLCLP\"}' --out=pubs.json", shell=True)
+
+# # Open JSON file and append to list
 # docs = []
-# with open('test.txt', 'r') as open_file:
+# with open('pubs.json', 'r') as open_file:
 # 	for n in open_file:
 # 		docs.append(json.loads(n))
 # 	open_file.close()
 
-# Index documents in ES instance
+# # Index documents in ES instance
 # for doc in docs:
 # 	es.index(index="mm", doc_type="docs", id=doc.pop('_id'), body=doc)
 
-def search(term):
-	name = es.search(index="mm", body={ "query": { "match": { "year": str(term)}}})
-	pprint.pprint(name['hits']['hits'])
+with open('categories.json', 'r') as open_file:
+	# for n in open_file:
+	algofile = json.load(open_file)
+	open_file.close()
 
-# elastictest.index_docs(testjson)
 
-search("2005")
+# Search for the paragraphs using the full term for the concept of method
+def search_by_paragraph_term(term):
+	q = es.search(index="mm", body={
+		"_source":["content.chapters.paragraphs"],  
+		"query": { "match_phrase": { "content.chapters.paragraphs": str(term)}},
+		"size":  40
+		})
 
+	return q
+
+
+# Search for the titles containing mentions of the term
+def search_for_title(term):
+	q = es.search(index="mm", body={
+		"_source":["title"],
+		"query": { "match_phrase": {"content.chapters.paragraphs": str(term)}},
+		"size":  40
+		})
+
+	res = []
+	for n in q['hits']['hits']:
+		res.append(n['_source']['title'])
+	
+	return res
+
+
+# Search for the paragraphs using the abbreviation for the concept of method
+def search_by_paragraph_abbrev(term, titles):
+	res = []
+	q = es.search(index="mm", body={
+		"_source":["content.chapters.paragraphs", "title"],  
+		"query": { "match_phrase": { "content.chapters.paragraphs": str(term)}},
+		"size":  40
+		})
+	
+	for doc in q:
+		# pprint.pprint(doc)
+		for n in doc:
+			pprint.pprint(n)
+		# 	if n['title'] == title:
+		# 		res.append(n)
+
+	pprint.pprint(res)
+
+
+# Get the abbreviated term
+def get_abbrev(term):
+	for n in algofile['categories']:
+		for o in n['Algorithms']:
+			if term == o['title']:
+				return o['abbreviation']
+
+
+# Search for a term
+def search_comprehensive(term):
+	# Retrieve abbreviation for term
+	abbrev = get_abbrev(term)
+	
+	titles = search_for_title(term)
+	paras_term = search_by_paragraph_term(term)
+	paras_abbrev = search_by_paragraph_abbrev(abbrev, titles)
+
+	pprint.pprint(paras_abbrev)
+
+
+search_comprehensive('The prosodic features related to pitch and energy failed to distinguish the valence emotions. The selected features discussed in Section 3.1 were quantized into feature vector 1 Y and mean feature vector 2 Y')
